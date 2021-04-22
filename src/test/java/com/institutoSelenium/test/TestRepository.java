126d29e2820f25ca0b4772e94439976b0e0bf98d
bsparnochia <brian.sparnochia@gmail.com>
2021-04-21 22:50:15 -0300

    Se actualizo el test para verificar consultas por apellido, nombre y apellido, id del alumno y por id de curso

--- src/test/java/com/institutoSelenium/test/TestRepository.java
+++ src/test/java/com/institutoSelenium/test/TestRepository.java
@@ -3,24 +3,48 @@ import com.institutoSelenium.connector.Connector;
 import com.institutoSelenium.model.Alumno;
 import com.institutoSelenium.repositories.interfaces.I_AlumnoRepository;
 import com.institutoSelenium.repositories.jdbc.AlumnoRepository;
+import java.util.List;
 
 public class TestRepository {
     public static void main(String[] args) {
         I_AlumnoRepository ar = new AlumnoRepository(Connector.getConnection());
         
-        Alumno alumno1 = new Alumno("brian","sparnochia",28,4);
+        Alumno alumno1 = new Alumno("brian","sparnochia",29,4);
         Alumno alumno2 = new Alumno("adrian","reinoso",50,10);
+        /* 1) */
+        List<Alumno> inscripciones = ar.getAll();
+        /* 2) */
         Alumno buscado = ar.getById(7);
+        /* 2) */
+        List<Alumno> listadoApellidos = ar.getLikeApellido("rojas");
+        /* 3) */
+        List<Alumno> listadoNombreApellido = ar.getLikeNombreApellido("tamara", "rojas");
         
-        //ar.save(alumno1);
-        //alumno.setApellido("AMSTRONG");
-        //ar.update(alumno);
-        //ar.remove(alumno);
+        /* Pruebas de guardado, actualización y eliminación en tabla ALUMNOS */
+        //ar.save(alumno2);
+        ar.update(alumno1);
+        ar.remove(alumno1);
         
+        System.out.println("1)\nLista de inscriptos actuales de todos los cursos");
+        for(Alumno alumno:inscripciones){
+            System.out.println(alumno.toString());
+        }
+        
+        System.out.println("2)\nBusqueda de Alumno por id ("+buscado.getId()+")");
         if (buscado.getId() == 0){
             System.out.println("El alumno no existe en la base de datos");
         }else{
             System.out.println(buscado.toString());
         }
+        
+        System.out.println("3)\nLista de coincidencias con el mismo apellido");
+        for(Alumno alumno:listadoApellidos){
+            System.out.println(alumno.toString());
+        }
+        
+        System.out.println("4)\nLista de coincidencias con el mismo nombre y apellido");
+        for (Alumno alumno : listadoNombreApellido) {
+            System.out.println(alumno.toString());
+        }   
     }
 }
