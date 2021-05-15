071d852c0d0738618e60a091556dba52100e9ecc
bsparnochia <brian.sparnochia@gmail.com>
2021-05-15 01:02:36 -0300

    Formato final y funcional para la de base de datos

--- src/main/java/com/institutoSelenium/sql/base.sql
+++ src/main/java/com/institutoSelenium/sql/base.sql
@@ -11,9 +11,9 @@ CREATE TABLE CURSOS(
 CREATE TABLE ALUMNOS(
     ID INT AUTO_INCREMENT PRIMARY KEY,
     NOMBRE VARCHAR(50) NOT NULL,
-    APELLIDO VARCHAR(50) NOT NULL,dadad
+    APELLIDO VARCHAR(50) NOT NULL,
     EDAD INT UNSIGNED,
-    IDCURSO INT NOT NULL,sdadas
+    IDCURSO INT NOT NULL,
     FOREIGN KEY (IDCURSO) REFERENCES CURSOS(ID)
 );
 -- ALTER TABLE ALUMNOS
@@ -23,20 +23,29 @@ CREATE TABLE ALUMNOS(
 
 # INSERCION DE CURSOS ---------------------------------------------------------------------------------------
 
-INSERT INTO CURSOS(ID, TITULO, PROFESOR, DIA, TURNO) VALUES(0,'Angular','Carlos Rios','MARTES','TARDE');
-INSERT INTO CURSOS(ID, TITULO, PROFESOR, DIA, TURNO) VALUES(0,'PHP', 'Eduardo Gomez','MIERCOLES','MAÑANA');
-INSERT INTO CURSOS(ID, TITULO, PROFESOR, DIA, TURNO) VALUES(0,'Photoshop','Lucia Azcuenaga','JUEVES','NOCHE');
-INSERT INTO CURSOS(ID, TITULO, PROFESOR, DIA, TURNO) VALUES(0,'C#','Arnold Schmidt','LUNES','NOCHE');
-INSERT INTO CURSOS(ID, TITULO, PROFESOR, DIA, TURNO) VALUES(0,'Python','Luciano Castro','VIERNES','MAÑANA');
+INSERT INTO CURSOS(ID, TITULO, PROFESOR, DIA, TURNO) VALUES
+    (0,'Angular','Carlos Rios','MARTES','TARDE'),
+    (0,'PHP', 'Eduardo Gomez','MIERCOLES','MAÑANA'),
+    (0,'Photoshop','Lucia Azcuenaga','JUEVES','NOCHE'),
+    (0,'C#','Arnold Schmidt','LUNES','NOCHE'),
+    (0,'Python','Luciano Castro','VIERNES','MAÑANA'),
+    (0,'Java Spring','Antonio Bernasconi','LUNES','NOCHE'),
+    (0,'Java Spring','Antonio Bernasconi','MARTES','TARDE'),
+    (0,'NodeJs','Irina Palacios','VIERNES','MAÑANA'),
+    (0,'Javascript','Liliana Ramos','MIERCOLES','TARDE'),
+    (0,'Introducción a base de datos','Andres García','LUNES','MAÑANA');
 
 # INSERCION DE ALUMNOS --------------------------------------------------------------------------------------
 
-INSERT INTO ALUMNOS(ID, NOMBRE, APELLIDO, EDAD, IDCURSO) VALUES (0,'PEPE','MUJICA',44,1);
-INSERT INTO ALUMNOS(ID, NOMBRE, APELLIDO, EDAD, IDCURSO) VALUES (0,'ANTONELLA','CIRILLO',32,4);
-INSERT INTO ALUMNOS(ID, NOMBRE, APELLIDO, EDAD, IDCURSO) VALUES (0,'DAMIAN','VENEGAS', 29, 5);
-INSERT INTO ALUMNOS(ID, NOMBRE, APELLIDO, EDAD, IDCURSO) VALUES (0,'MARIANA', 'MORENO', 17,5);
-INSERT INTO ALUMNOS(ID, NOMBRE, APELLIDO, EDAD, IDCURSO) VALUES (0,'CARLA','VALENZUELA', 24, 3);
-INSERT INTO ALUMNOS(ID, NOMBRE, APELLIDO, EDAD, IDCURSO) VALUES (0,'TAMARA','ROJAS',22,2);
-INSERT INTO ALUMNOS(ID, NOMBRE, APELLIDO, EDAD, IDCURSO) VALUES (0,'VIVIANA','ROJAS',22,2);
-
-SELECT * FROM ALUMNOS
+INSERT INTO ALUMNOS(ID, NOMBRE, APELLIDO, EDAD, IDCURSO) VALUES 
+    (0,'José','Mujica',44,1),
+    (0,'Antonella','Cirillo',32,4),
+    (0,'Damián','Venegas', 29, 5),
+    (0,'Mariana', 'Fabiani', 17,5),
+    (0,'Sabrina','Valenzuela', 24, 3),
+    (0,'Tamara','Rojas',22,2),
+    (0,'Viviana','Rojas',22,2),
+    (0,'Rosario','García',25,7),
+    (0,'Mauricio','Rivera',28,6),
+    (0,'Joaquín','Sabina',33,10),
+    (0,'fabian','Pastorutti',40,9);
